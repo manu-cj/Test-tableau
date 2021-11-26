a11397877e13029271840f6d3e9b3ae16f7715b6
manucarenjot <carenjotje@gmail.com>
2021-11-26 09:49:08 +0100

    Test tableau

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 let myArray = [" les gars", " les filles", " la classe", " tout le monde", " world"];
-
+console.log(myArray)
 document.getElementById('p1').innerHTML += myArray[2];
+myArray.splice(2, 5);
+console.log(myArray);
